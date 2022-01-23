0999f8824d15bcb82f39724bfa294458faadaedb
Dinelle Zipser <dinellezipser@gmail.com>
2022-01-23 12:18:35 -0800

    first commit

---
+++ messages.js
@@ -0,0 +1,24 @@
+const meal = {
+    appetizer: ['onion rings', 'chips and salsa', 'tater tots', 'fries', 'nachos', 'salad'],
+    main: ['steak', 'salmon', 'pasta', 'sandwich', 'burrito', 'pizza'],
+    dessert: ['ice cream', 'cake', 'cheesecake', 'cookies', 'brownies'] 
+};
+
+for (const [key, value] of Object.entries(meal)) {
+  let i = Math.floor(Math.random() * key.length); 
+
+  switch (key) {
+      
+  case 'appetizer':
+  console.log(`Start with: ${value[i]}`);
+  break;
+
+  case 'main':
+  console.log(`Your meal will be: ${value[i]}`);
+  break;
+
+  case 'dessert':
+  console.log(`Your dessert will be: ${value[i]}`);
+  break;
+  }
+}
